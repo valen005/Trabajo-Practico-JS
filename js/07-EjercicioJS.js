0909724ff50a41ac7ee2e2f22d8cefda8794921b
Valentina Acosta <valuchi005@gmail.com>
2023-10-30 23:28:14 -0300

    Ahora si terminado el punto 7

--- js/07-EjercicioJS.js
+++ js/07-EjercicioJS.js
@@ -2,7 +2,7 @@ let numero1 = parseFloat(prompt("Introduce un número: "));
 let numero2 = parseFloat(prompt("Introduce un número: "));
 let numero3 = parseFloat(prompt("Introduce un número: "));
 
-if (numero1 > numero2 && numero1 > nu) {
+if (numero1 > numero2 && numero1 > numero3) {
   document.write("El número, " + numero1 + ", es el mayor.");
 } else if (numero2 > numero1 && numero2 > numero3) {
   document.write("El número, " + numero2 + ", es el mayor.");
