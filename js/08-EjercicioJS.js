a633c6af68102f456540416937ed9572d24e4158
Valentina Acosta <valuchi005@gmail.com>
2023-10-31 20:00:50 -0300

    Terminado el punto 8

--- js/08-EjercicioJS.js
+++ js/08-EjercicioJS.js
@@ -2,7 +2,7 @@
 let numero = parseInt(prompt("Introduce un número:"));
 
 
-if (numero % 2 === 0) {
+if (numero % 2 == 0) {
     alert("El número " + numero + " es divisible por 2 (es par).");
 } else {
     document.write("El número " + numero + " no es divisible por 2 (es impar).");
